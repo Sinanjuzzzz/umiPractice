984999b7d704cf8591e7052b0ac85aa68defd435
Sinanjuzzzz <yaojunjie@ewell.cc>
2019-09-11 10:28:21 +0800

    发票信息

--- src/pages/Form/InvoiceForm.js
+++ src/pages/Form/InvoiceForm.js
@@ -1,4 +1,5 @@
-import { Form, Switch } from 'antd'
+import React, { Fragment } from 'react'
+import { Form, Switch, Input, Icon } from 'antd'
 
 function InvoiceForm(props) {
 
@@ -10,11 +11,35 @@ function InvoiceForm(props) {
   }
 
   const invoiceFormItem = () => {
-    if(getFieldValue('invoiceSwitch')){
-      return(
-        <div>
-          发票
-        </div>
+    if (getFieldValue('invoiceSwitch')) {
+      return (
+        <Fragment>
+          <Form.Item label='发票抬头' style={{ display: "flex", position: "relative", margin: "0 auto" }}>
+            {getFieldDecorator('invoiceRise', {
+              rules: [{ required: true, message: '请输入发票抬头!' }]
+            })(
+              <Input placeholder='请输入' style={{ position: 'relative', width: '300px' }}></Input>
+            )}
+          </Form.Item>
+          <Form.Item label='纳税人识别号' style={{ display: "flex", position: "relative", margin: "0 auto" }}>
+            {getFieldDecorator('taxpayerIdNumber', {
+              rules: [{ required: true, message: '请输入纳税人识别号!' }]
+            })(
+              <Input placeholder='请输入' style={{ position: 'relative', width: '300px' }}></Input>
+            )}
+          </Form.Item>
+          <Form.Item label='电话号码' style={{ display: "flex", position: "relative", margin: "0 auto" }}>
+            {getFieldDecorator('phoneNumber', {
+              rules: [{ required: true, message: '请输入电话号码!' }]
+            })(
+              <Fragment>
+                <Input placeholder='086' style={{ position: 'relative', width: '100px' }}></Input>
+                <Icon type="dash" style={{margin: "0px 5px 0px 5px"}} />
+                <Input placeholder='请输入' style={{ position: 'relative', width: '300px' }}></Input>
+              </Fragment>
+            )}
+          </Form.Item>
+        </Fragment>
       )
     }
     return null
